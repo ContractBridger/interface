88c7c2a97ba7fd3c243106be1c85b91caca308a2
AjayiMike <ajayimikeofficial@gmail.com>
2023-10-20 23:38:56 +0100

    feat: make SelectChainDropdown component controlled

--- components/SelectChainDropdown.tsx
+++ components/SelectChainDropdown.tsx
@@ -3,14 +3,15 @@ import React, { FC } from "react";
 import { useNetwork } from "wagmi";
 
 const SelectChainDropdown: FC<{
-  selectedChain: number;
   handleChange: (chain: any) => void;
-}> = ({ selectedChain, handleChange }) => {
+}> = ({ handleChange }) => {
   const network = useNetwork();
   return (
     <Select label="Select Chain" onChange={handleChange}>
       {network.chains.map((chain) => (
-        <Option key={chain.id}>{chain.name}</Option>
+        <Option key={chain.id} value={String(chain.id)}>
+          {chain.name}
+        </Option>
       ))}
     </Select>
   );
