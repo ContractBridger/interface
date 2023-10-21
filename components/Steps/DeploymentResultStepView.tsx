8c8d2da6450ecb1dc265f4da6a2c46366235db73
AjayiMike <ajayimikeofficial@gmail.com>
2023-10-21 14:09:38 +0100

    feat: remove dummy data and render real deployment result

--- components/Steps/DeploymentResultStepView.tsx
+++ components/Steps/DeploymentResultStepView.tsx
@@ -1,46 +1,60 @@
 "use client";
 import { Card, Typography } from "@material-tailwind/react";
-import React from "react";
+import { ContractTransactionReceipt } from "ethers";
+import React, { FC } from "react";
+import { toast } from "react-toastify";
+import { useNetwork } from "wagmi";
 
-const DeploymentResultStepView = () => {
+const DeploymentResultStepView: FC<{
+  data: ContractTransactionReceipt | null;
+}> = ({ data }) => {
+  const network = useNetwork();
+
+  if (!data) {
+    toast.error("no contract deployment result found");
+    return;
+  }
   return (
     <Card color="transparent" shadow={false}>
       <Typography variant="h4" color="blue-gray">
         Deployment Result
       </Typography>
       <div className="flex flex-col gap-4 mt-8">
-        <div className="flex flex-col mb-4">
-          <span className="text-sm">Status</span>
-          <span>Success</span>
-        </div>
+        {data.status === 1 ? (
+          <div className="flex flex-col mb-4">
+            <span className="text-sm">Status</span>
+            <span className="text-green-800 font-bold">Success &#10003;</span>
+          </div>
+        ) : (
+          <div className="flex flex-col mb-4">
+            <span className="text-sm">Status</span>
+            <span className="text-red-800 font-bold">Failled &#x2718;</span>
+          </div>
+        )}
 
         <div className="flex flex-col mb-4">
-          <span className="text-sm">Transaction Hash</span>
-          <a
-            href="https://etherscan.io/tx/0x5b73e239c55d790e3c9c3bbb84092652db01bb8dbf49ccc9e4a318470419d9a0"
-            target="_blank"
-          >
-            0x5b73e239c55d790e3c9c3bbb84092652db01bb8dbf49ccc9e4a318470419d9a0
-          </a>
-        </div>
-
-        <div className="flex flex-col mb-4">
-          <span className="text-sm">Deployed address</span>
-          <span>0x543b4b074CC6313ef811F4c300166cb1e81567DE</span>
+          <span className="text-sm">Deployed to</span>
+          <span className="font-bold">{data.contractAddress}</span>
         </div>
 
         <div className="flex flex-col mb-4">
           <span className="text-sm">Deployer</span>
-          <span>0xd5E4484326EB3Dd5FBbd5Def6d02aFE817fD4684</span>
+          <span className="font-bold">{data.from}</span>
         </div>
 
         <div className="flex flex-col mb-4">
-          <span className="text-sm">Explorer Link</span>
+          <span className="text-sm">Tx Hash</span>
+          <span className="font-bold">{data.hash}</span>
+        </div>
+
+        <div className="flex flex-col mb-4">
+          <span className="text-sm">See more</span>
           <a
-            href="https://etherscan.io/address/0x5e227ad1969ea493b43f840cff78d08a6fc17796"
+            href={`${network.chain.blockExplorers.default.url}/tx/${data.hash}`}
             target="_blank"
+            className="font-bold text-green-800 hover:underline"
           >
-            0x5e22...7796
+            Explorer Link &#10138;
           </a>
         </div>
       </div>
